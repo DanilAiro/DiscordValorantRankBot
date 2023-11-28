a09ea771e132abe4a8f1e2b2c390882fc5739fed
DanilAiro <danchik.b@mail.ru>
2023-11-29 00:40:34 +0300

    v. 1.0.1 # spliter

--- Discord.cs
+++ Discord.cs
@@ -1,10 +1,10 @@
 using DSharpPlus;
-using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 
 internal class Discord
 {
   private static DiscordClient? Client { get; set; }
+
   public static async Task WaitMessage()
   {
     var discordConfig = new DiscordConfiguration()
@@ -27,7 +27,7 @@ internal class Discord
       if (e.Message.Channel.Name == "ранг" || e.Message.Channel.Name == "rank")
       {
         Console.WriteLine(e.Message + " - " + dt);
-        string[] strs = e.Message.Content.Split(' ');
+        string[] strs = e.Message.Content.Split('#');
 
         string rank = String.Empty; ;
 
@@ -40,16 +40,13 @@ internal class Discord
           rank = Valorant.GetRank(strs[2], strs[0], strs[1]);
         }
 
-        System.Console.WriteLine(rank);
+        Console.WriteLine(rank);
 
         if (!String.IsNullOrEmpty(rank))
         {
           UpdateRole(e, rank);
         }
-
-
       }
-
     };
 
     await Client.ConnectAsync();
@@ -63,10 +60,9 @@ internal class Discord
 
   private static void UpdateRole(MessageCreateEventArgs e, string newRank)
   {
-    // RemoveRole(e.Guild.Members[e.Author.Id]);
     var member = e.Guild.Members[e.Author.Id];
 
-    string[] ranks = {
+    string[] ranks = [
       "Iron 1",
       "Iron 2",
       "Iron 3",
@@ -92,7 +88,7 @@ internal class Discord
       "Immortal 2",
       "Immortal 3",
       "Radiant"
-    };
+    ];
 
     var roles = member.Roles;
 
@@ -120,56 +116,5 @@ internal class Discord
         break;
       }
     }
-  }
-
-  private static void RemoveRole(DiscordMember member)
-  {
-    string[] ranks = {
-      "Iron 1",
-      "Iron 2",
-      "Iron 3",
-      "Bronze 1",
-      "Bronze 2",
-      "Bronze 3",
-      "Silver 1",
-      "Silver 2",
-      "Silver 3",
-      "Gold 1",
-      "Gold 2",
-      "Gold 3",
-      "Platinum 1",
-      "Platinum 2",
-      "Platinum 3",
-      "Diamond 1",
-      "Diamond 2",
-      "Diamond 3",
-      "Ascedant 1",
-      "Ascedant 2",
-      "Ascedant 3",
-      "Immortal 1",
-      "Immortal 2",
-      "Immortal 3",
-      "Radiant"
-    };
-
-    var roles = member.Roles;
-
-    foreach (var role in roles)
-    {
-      foreach (var rank in ranks)
-      {
-        if (role.Name == rank)
-        {
-          member.RevokeRoleAsync(role);
-          Console.WriteLine($"{member.DisplayName} - remove - {role.Name}");
-        }
-      }
-    }
-  }
-
-    private static async void GiveRole(DiscordMember member, DiscordRole role)
-    {
-      await member.GrantRoleAsync(role);
-      Console.WriteLine($"{member.DisplayName} - gives - {role.Name}");
-    }
+  } 
 }
